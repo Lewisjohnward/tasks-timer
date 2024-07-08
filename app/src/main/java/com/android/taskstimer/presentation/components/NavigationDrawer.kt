84e8fdfa0793b91f17f22b0f722b69f3caa3e901
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-07-08 20:25:34 +0100

    Styling and NavigationDrawerItems

--- app/src/main/java/com/android/taskstimer/presentation/components/NavigationDrawer.kt
+++ app/src/main/java/com/android/taskstimer/presentation/components/NavigationDrawer.kt
@@ -1,22 +1,26 @@
 package com.android.taskstimer.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.android.taskstimer.ui.theme.BackgroundDarkGray
 
 @Composable
 fun NavigationDrawer() {
@@ -24,19 +28,78 @@ fun NavigationDrawer() {
         modifier = Modifier
             .fillMaxWidth(0.8f)
             .fillMaxHeight()
-            .background(Color(0xFF536878))
+            .background(BackgroundDarkGray),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Button(
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Red
-            ),
-            shape = RoundedCornerShape(0.dp)
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = null)
-                Text(text = "Stopwatch")
-            }
+        Column {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "Menu",
+                color = Color(0x99FFFFFF),
+                fontWeight = FontWeight.Bold
+            )
+            NavigationDrawerItem(
+                label = { Text(text = "Stopwatch") },
+                icon = { Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = null) },
+                selected = false,
+                shape = RoundedCornerShape(0.dp),
+                onClick = { /*TODO*/ },
+                colors = NavigationDrawerItemDefaults.colors(
+                    selectedContainerColor = Color.Red,
+                    unselectedContainerColor = Color.Transparent,
+//                selectedIconColor =,
+                    unselectedIconColor = Color.White,
+//                selectedTextColor =,
+                    unselectedTextColor = Color.White,
+//                selectedBadgeColor =,
+//                unselectedBadgeColor =
+                )
+            )
+            NavigationDrawerItem(
+                label = { Text(text = "Simple Timer") },
+                icon = { Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = null) },
+                selected = true,
+                shape = RoundedCornerShape(0.dp),
+                onClick = { /*TODO*/ }
+            )
         }
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color.Gray)
+        )
+        Column {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "Boards",
+                color = Color(0x99FFFFFF),
+                fontWeight = FontWeight.Bold
+            )
+            NavigationDrawerItem(
+                label = { Text(text = "Basic productivity") },
+                icon = { Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = null) },
+                selected = false,
+                shape = RoundedCornerShape(0.dp),
+                onClick = { /*TODO*/ },
+                colors = NavigationDrawerItemDefaults.colors(
+                    selectedContainerColor = Color.Red,
+                    unselectedContainerColor = Color.Transparent,
+//                selectedIconColor =,
+                    unselectedIconColor = Color.White,
+//                selectedTextColor =,
+                    unselectedTextColor = Color.White,
+//                selectedBadgeColor =,
+//                unselectedBadgeColor =
+                )
+            )
+        }
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color.Gray)
+        )
     }
 }
+
