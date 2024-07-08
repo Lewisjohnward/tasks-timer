cd9205a0fba07cdbc7cb269db7e9732ec4403ba0
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-07-08 20:43:52 +0100

    Changed background

--- app/src/main/java/com/android/taskstimer/presentation/components/TimerTopBar.kt
+++ app/src/main/java/com/android/taskstimer/presentation/components/TimerTopBar.kt
@@ -1,5 +1,6 @@
 package com.android.taskstimer.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -7,12 +8,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.android.taskstimer.ui.theme.BackgroundDarkGray
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,7 +35,13 @@ fun TimerTopBar(
                 fontSize = 25.sp
             )
         },
-        modifier = modifier,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = BackgroundDarkGray,
+            navigationIconContentColor = Color.White,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.Red
+
+        ),
         scrollBehavior = scrollBehavior,
         navigationIcon = {
             if (displayIcon) {
