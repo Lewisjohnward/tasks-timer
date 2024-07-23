62e3aee46f853563bce013236b9d393c5e796549
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-07-23 15:14:39 +0100

    Style InputDialog

--- app/src/main/java/com/android/taskstimer/tasks_timer/presentation/components/InputDialog.kt
+++ app/src/main/java/com/android/taskstimer/tasks_timer/presentation/components/InputDialog.kt
@@ -1,6 +1,7 @@
 package com.android.taskstimer.tasks_timer.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,27 +9,36 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import com.android.taskstimer.core.presentation.ui.theme.BackgroundDarkGray
+import com.android.taskstimer.core.presentation.ui.theme.TasksTimerTheme
 import com.android.taskstimer.tasks_timer.presentation.HomeScreenEvent
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputDialog(
-    myNewBoardName: String = "My awesome new board name",
+    myNewBoardName: String = "My awesome new board",
     cancel: () -> Unit = {},
     onEvent: (HomeScreenEvent) -> Unit = {}
 ) {
+
+
     AlertDialog(
         onDismissRequest = {},
     ) {
@@ -36,17 +46,54 @@ fun InputDialog(
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(5.dp))
-                .background(Color(0xFFFFFFFF))
+                .background(BackgroundDarkGray)
                 .padding(20.dp)
         ) {
-            Column {
-                TextField(value = myNewBoardName, onValueChange = {})
-                Row {
-                    Button(onClick = { onEvent(HomeScreenEvent.CreateBoard(myNewBoardName)) }) {
-                        Text(text = "confirm")
+            Column(
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                Text(
+                    text = "New board name",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+                TextField(
+                    value = myNewBoardName,
+                    onValueChange = {},
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "Add icon"
+                        )
+                    },
+                    singleLine = true,
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        focusedLeadingIconColor = Color(0x99FFFFFF),
+                        unfocusedLeadingIconColor = Color(0x44FFFFFF),
+                        cursorColor = Color.White
+
+                        ),
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(onClick = { onEvent(HomeScreenEvent.CreateBoard(myNewBoardName)) }) {
+                        Text(
+                            text = "confirm",
+                            color = Color(0xFFFF9B88)
+                        )
                     }
-                    Button(onClick = { cancel() }) {
-                        Text(text = "Cancel")
+                    TextButton(onClick = { cancel() }) {
+                        Text(
+                            text = "Cancel",
+                            color = Color(0xFFFF9B88)
+
+                        )
                     }
                 }
 
@@ -58,9 +105,11 @@ fun InputDialog(
 @Preview(showBackground = true)
 @Composable
 fun InputDialogPreview() {
-    Column(
-        Modifier.fillMaxSize()
-    ) {
-        InputDialog()
+    TasksTimerTheme {
+        Column(
+            Modifier.fillMaxSize()
+        ) {
+            InputDialog()
+        }
     }
 }
