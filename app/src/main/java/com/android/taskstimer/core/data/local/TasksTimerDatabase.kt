b0bbb6504ba70be97932a94b70deb757644a8ba7
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-09-14 10:31:39 +0100

    Remove unused db code

--- app/src/main/java/com/android/taskstimer/core/data/local/TasksTimerDatabase.kt
+++ app/src/main/java/com/android/taskstimer/core/data/local/TasksTimerDatabase.kt
@@ -17,30 +17,3 @@ abstract class TasksTimerDatabase: RoomDatabase(){
     abstract val timerDao: TimerDao
     abstract val boardDao: BoardDao
 }
-
-//@Database(entities = [Timer::class, Board::class], version = 1, exportSchema = false)
-//abstract class TasksTimerDatabase : RoomDatabase() {
-//
-//    abstract fun timerDao(): TimerDao
-//    abstract fun boardDao(): BoardDao
-//
-//    companion object {
-//        @Volatile
-//        private var Instance: TasksTimerDatabase? = null
-//
-//        fun getDatabase(context: Context): TasksTimerDatabase {
-//            // if the Instance is not null, return it, otherwise create a new database instance.
-//            return Instance ?: synchronized(this) {
-//                Room.databaseBuilder(context, TasksTimerDatabase::class.java, "timer_database")
-//                    /**
-//                     * Setting this option in your app's database builder means that Room
-//                     * permanently deletes all data from the tables in your database when it
-//                     * attempts to perform a migration with no defined migration path.
-//                     */
-//                    .fallbackToDestructiveMigration()
-//                    .build()
-//                    .also { Instance = it }
-//            }
-//        }
-//    }
-//}
