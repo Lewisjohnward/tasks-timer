39b29f771b11dddb79b88d9504786ad762d90844
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-09-18 11:48:43 +0100

    Remove padding from LazyColumn

--- app/src/main/java/com/android/taskstimer/tasks_timer/presentation/HomeScreen.kt
+++ app/src/main/java/com/android/taskstimer/tasks_timer/presentation/HomeScreen.kt
@@ -3,8 +3,10 @@ package com.android.taskstimer.tasks_timer.presentation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +17,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
@@ -164,7 +167,7 @@ private fun HomeScreenContent(
                             modifier = Modifier
 //                .weight(0.8f)
                                 .fillMaxWidth()
-                                .padding(25.dp),
+                                .padding(top = 0.dp, start = 20.dp, end = 10.dp),
                             verticalArrangement = Arrangement.spacedBy(10.dp)
                         ) {
                             itemsIndexed(tasksTimerService.state.value.timers) { index, timer ->
@@ -189,6 +192,9 @@ private fun HomeScreenContent(
                                         )
                                     }
                                 )
+                            }
+                            item {
+                                Spacer(modifier = Modifier.fillMaxWidth().height(75.dp))
                             }
                         }
                 }
