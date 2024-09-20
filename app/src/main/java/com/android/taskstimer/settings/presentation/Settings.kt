ba8f61747d023fbc0bd4859380b49330b918866f
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-09-20 08:21:40 +0100

    Create SettingsScreen

--- app/src/main/java/com/android/taskstimer/settings/presentation/Settings.kt
+++ app/src/main/java/com/android/taskstimer/settings/presentation/Settings.kt
@@ -1,13 +1,49 @@
 package com.android.taskstimer.settings.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android.taskstimer.R
 import com.android.taskstimer.core.presentation.navigation.NavigationDestination
 import com.android.taskstimer.core.presentation.ui.theme.BackgroundDarkGray
+import com.android.taskstimer.core.presentation.ui.theme.Green
+import com.android.taskstimer.core.presentation.ui.theme.TasksTimerTheme
+import com.android.taskstimer.tasks_timer.presentation.components.TimerTopBar
 
 object SettingsDestination : NavigationDestination {
     override val route = "settings"
@@ -15,13 +51,205 @@ object SettingsDestination : NavigationDestination {
 }
 
 @Composable
-fun SettingsScreen(navigateBack: () -> Boolean) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(BackgroundDarkGray),
-    ) {
-        Text(text = "settings page")
+fun SettingsScreen(navigateBack: () -> Unit) {
+    SettingsScreenContent(
+        navigateBack = navigateBack
+    )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SettingsScreenContent(
+    navigateBack: () -> Unit = {}
+) {
+    var displayDialog: Boolean by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TimerTopBar(
+                title = "Settings",
+                iconOnclick = navigateBack
+            )
+        },
+        containerColor = BackgroundDarkGray
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            SettingsItem(
+                text = stringResource(R.string.ignore_silent_mode),
+                description = stringResource(R.string.ignore_silent_mode_description),
+                input = {
+                    Switch(
+                        onClick = {},
+                        checked = true
+                    )
+                },
+            )
+            SettingsItem(
+                text = stringResource(R.string.time_interval_between_timers),
+                description = stringResource(R.string.time_interval_between_timers_description),
+                input = {
+                    Time(
+                        onClick = {displayDialog = true},
+                        value = 5.toString()
+                    )
+                }
+            )
+            SettingsItem(
+                text = stringResource(R.string.use_media_volume_when_headphones_are_connected),
+                description = stringResource(R.string.use_media_volume_when_headphones_are_connected_description),
+                input = {
+                    Switch(
+                        onClick = {},
+                        checked = false
+                    )
+                }
+            )
+        }
+        if (displayDialog) {
+            Dialog(dismiss = { displayDialog = false })
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Dialog(dismiss: () -> Unit) {
+
+    CompositionLocalProvider(value = LocalMinimumInteractiveComponentEnforcement provides false) {
+        BasicAlertDialog(
+            onDismissRequest = {dismiss()}
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(50.dp))
+                    .background(Color(0xFFFFFFFF))
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    IconButton(
+                        modifier = Modifier.size(100.dp),
+                        onClick = {}) {
+                        Icon(
+                            modifier = Modifier
+                                .rotate(90f)
+                                .fillMaxSize(),
+                            imageVector = Icons.Filled.ChevronLeft,
+                            contentDescription = "Increase",
+                        )
+
+                    }
+                    Text(
+                        text = "5s",
+                        fontSize = 75.sp,
+                    )
+
+                    IconButton(
+                        modifier = Modifier
+                            .size(100.dp),
+                        onClick = {}
+                    ) {
+                        Icon(
+                            modifier = Modifier
+                                .rotate(-90f)
+                                .fillMaxSize(),
+                            imageVector = Icons.Filled.ChevronLeft,
+                            contentDescription = "Decrease"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun SettingsItem(
+    text: String,
+    description: String,
+    input: @Composable () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.weight(0.5f),
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            Text(
+                text = text,
+                fontSize = 18.sp,
+                color = Color.White,
+            )
+            Text(
+                text = description,
+                fontSize = 14.sp,
+                color = Color.White.copy(alpha = 0.5f),
+            )
+        }
+        input()
+    }
+}
+
+@Composable
+private fun ColumnScope.Switch(
+    onClick: () -> Unit,
+    checked: Boolean
+) {
+    Switch(
+        modifier = Modifier.weight(0.1f),
+        checked = checked,
+        onCheckedChange = { onClick() },
+        colors = SwitchDefaults.colors(
+            checkedTrackColor = Green
+        )
+    )
+}
+
+@Composable
+private fun ColumnScope.Time(
+    onClick: () -> Unit,
+    value: String
+) {
+    TextButton(
+        modifier = Modifier.weight(0.1f),
+        onClick = { onClick() },
+    ) {
+        Row(
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Text(
+                text = value,
+                fontSize = 30.sp,
+                color = Color.White
+            )
+            Text(
+                text = "s",
+                fontSize = 15.sp,
+                color = Color.White
+            )
+
+        }
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun SettingsScreenPreview() {
+    TasksTimerTheme {
+        SettingsScreenContent()
     }
 }
